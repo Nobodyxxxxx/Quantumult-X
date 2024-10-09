d7e712e0e6ee411ddbbb492b4a210cbb3da2c6cc
NobodyXxxxxxxx <104611963+Nobodyxxxxx@users.noreply.github.com>
2024-10-10 00:05:24 +0800

    更新 jingdong.js

--- Scripts/jingdong.js
+++ Scripts/jingdong.js
@@ -117,9 +117,9 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
           // 底部工具栏
           if (floor?.data?.nodes?.length > 0) {
             const sortLists = [
-              "applezhushou", // apple助手 1-1-1
+              //"applezhushou", // apple助手 1-1-1
               "lingjindouxin", // 签到领豆 1-1-2
-              "dongdongnongchangxin", // 京东农场 1-1-3
+              //"dongdongnongchangxin", // 京东农场 1-1-3
               "chongwangwang", // 宠汪汪 1-1-4
               "kehufuwu", // 客户服务 1-2-1
               "xianzhiguanjia", // 闲置换钱 1-2-2
