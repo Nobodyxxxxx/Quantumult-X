186af1484902b77ff8ace3892523673996a19c72
NobodyXxxxxxxx <104611963+Nobodyxxxxx@users.noreply.github.com>
2024-10-09 22:48:51 +0800

    Update jingdong.js

--- Scripts/jingdong.js
+++ Scripts/jingdong.js
@@ -112,20 +112,20 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
             delete floor.data.floatLayer;
           }
         } else if (floor?.mId === "iconToolFloor") {
-         /* 
+         
           // 底部工具栏
           if (floor?.data?.nodes?.length > 0) {
             const sortLists = [
-              "applezhushou", // apple助手 1-1-1
+              //"applezhushou", // apple助手 1-1-1
               "lingjindouxin", // 签到领豆 1-1-2
-              "dongdongnongchangxin", // 京东农场 1-1-3
-              "chongwangwang", // 宠汪汪 1-1-4
+              //"dongdongnongchangxin", // 京东农场 1-1-3
+              //"chongwangwang", // 宠汪汪 1-1-4
               "kehufuwu", // 客户服务 1-2-1
-              "xianzhiguanjia", // 闲置换钱 1-2-2
-              "wenyisheng", // 问医生 1-2-3
+              //"xianzhiguanjia", // 闲置换钱 1-2-2
+              //"wenyisheng", // 问医生 1-2-3
               "jijianfuwu", // 寄件服务 1-2-5
               "zhuanzuanhongbao", // 天天赚红包 2-2-1
-              "huanletaojin" // 欢乐淘金 2-2-2
+              //"huanletaojin" // 欢乐淘金 2-2-2
             ];
             let node = floor.data.nodes;
             if (node?.[0]?.length > 0) {
@@ -141,7 +141,7 @@ if (url.includes("functionId=deliverLayer") || url.includes("functionId=orderTra
                 .sort((a, b) => sortLists.indexOf(a?.functionId) - sortLists.indexOf(b?.functionId));
             }
           }
-*/
+
         } else if (floor?.mId === "orderIdFloor") {
           if (floor?.data?.commentRemindInfo?.infos?.length > 0) {
             // 发布评价的提醒
