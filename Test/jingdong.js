c7ee9bc8044d099cccf6a8be1f1eb5275d6be4ab
NobodyXxxxxxxx <104611963+Nobodyxxxxx@users.noreply.github.com>
2024-10-13 01:45:35 +0800

    更新 jingdong.js

--- Test/jingdong.js
+++ Test/jingdong.js
@@ -1,29 +1,20 @@
-let url = $request.url;
-let body = $response.body;
-let obj = JSON.parse(body);
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
 
 if (url.includes("/client.action?functionId=readCustomSurfaceList")) {
-  // 调试: 输出 dark 模式下的导航项
+  // 检查 modeMap 中的 dark 和 normal 模式的底部导航栏
   if (obj?.result?.modeMap?.dark?.navigationAll?.length > 0) {
-    $notify("调试信息", "过滤前 dark 模式下的导航项", JSON.stringify(obj.result.modeMap.dark.navigationAll));
-    
-    // 进行过滤
+    // 过滤掉“逛”和“新品” (find 和 new)
     obj.result.modeMap.dark.navigationAll = obj.result.modeMap.dark.navigationAll.filter((i) => !/(find|new)/.test(i?.functionId));
-
-    // 调试: 输出过滤后的导航项
-    $notify("调试信息", "过滤后 dark 模式下的导航项", JSON.stringify(obj.result.modeMap.dark.navigationAll));
   }
-
-  // 调试: 输出 normal 模式下的导航项
+  
   if (obj?.result?.modeMap?.normal?.navigationAll?.length > 0) {
-    $notify("调试信息", "过滤前 normal 模式下的导航项", JSON.stringify(obj.result.modeMap.normal.navigationAll));
-
-    // 进行过滤
+    // 同样过滤掉 normal 模式下的“逛”和“新品”
     obj.result.modeMap.normal.navigationAll = obj.result.modeMap.normal.navigationAll.filter((i) => !/(find|new)/.test(i?.functionId));
-
-    // 调试: 输出过滤后的导航项
-    $notify("调试信息", "过滤后 normal 模式下的导航项", JSON.stringify(obj.result.modeMap.normal.navigationAll));
   }
-
-  $done({ body: JSON.stringify(obj) });
+  
+  
 }
+
+$done({ body: JSON.stringify(obj) });
