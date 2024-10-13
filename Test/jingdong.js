8ecf71d30354b93e2cc6cbb4c4e58040ad1baf41
NobodyXxxxxxxx <104611963+Nobodyxxxxx@users.noreply.github.com>
2024-10-13 16:28:20 +0800

    更新 jingdong.js

--- Test/jingdong.js
+++ Test/jingdong.js
@@ -1,20 +1,20 @@
-const url = $request.url;
-if (!$response.body) $done({});
-let obj = JSON.parse($response.body);
+let body = JSON.parse($response.body);
 
-if (url.includes("/client.action?functionId=readCustomSurfaceList")) {
-  // 检查 modeMap 中的 dark 和 normal 模式的底部导航栏
-  if (obj?.result?.modeMap?.dark?.navigationAll?.length > 0) {
-    // 过滤掉“逛”和“新品” (find 和 new)
-    obj.result.modeMap.dark.navigationAll = obj.result.modeMap.dark.navigationAll.filter((i) => !/(find|new)/.test(i?.functionId));
-  }
-  
-  if (obj?.result?.modeMap?.normal?.navigationAll?.length > 0) {
-    // 同样过滤掉 normal 模式下的“逛”和“新品”
-    obj.result.modeMap.normal.navigationAll = obj.result.modeMap.normal.navigationAll.filter((i) => !/(find|new)/.test(i?.functionId));
-  }
-  
-  
+// 修改card_items
+if (body.card_items && body.card_items.length > 0) {
+  body.card_items[0].priority_display = false;
+  body.card_items[0].is_show = false;
+}
+body.preview_guide = null;
+
+// 删除reward_ad_banner
+if (body.reward_ad_banner) {
+  delete body.reward_ad_banner;
 }
 
-$done({ body: JSON.stringify(obj) });
+// 删除blocks
+if (body.blocks && body.blocks.length > 1) {
+  delete body.blocks[1];
+}
+
+$done({body: JSON.stringify(bo
